b539da07c00af87db26d9e75f76af25ad55dc1d5
mac <chukwuekeuzoma@gmail.com>
2021-08-06 23:41:37 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FetchWeather } from './Api/FetchWeather';
+
+export default function App(props) {
+    return (
+        <div>
+            App
+        </div>
+    );
+}
